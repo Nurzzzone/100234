b821779602001d86f9a5e3e2a9a8dc65a17d3963
nurdaulet <nurzzzone@gmail.com>
2022-07-27 17:50:48 +0600

    refactor: updated vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,10 @@ Vue.component('v-table-data', require('./components/Table/TableData.vue').defaul
 Vue.component('v-table-tools', require('./components/Table/TableTools.vue').default);
 Vue.component('v-pagination', require('./components/Pagination/Pagination.vue').default);
 Vue.component('v-sidebar-search', require('./components/Sidebar/SidebarSearch.vue').default);
+Vue.component('v-sidebar-dropdown', require('./components/Sidebar/MenuDropdown.vue').default);
+Vue.component('v-sidebar-link', require('./components/Sidebar/MenuLink.vue').default);
+Vue.component('v-sidebar-title', require('./components/Sidebar/MenuTitle.vue').default);
+Vue.component('v-sidebar-menu', require('./components/Sidebar/Menu.vue').default);
 
 Vue.component('v-form', require('./components/Form/Form.vue').default);
 
